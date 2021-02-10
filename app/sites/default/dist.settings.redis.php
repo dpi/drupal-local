a8b0586fb8c4213e95517046e3741acea78b7fee
dpi <pro@danielph.in>
2021-02-10 16:41:45 +0800

    Conditionally load Redis settings and services by whether executed by PHPUnit

--- app/sites/default/dist.settings.redis.php
+++ app/sites/default/dist.settings.redis.php
@@ -4,6 +4,11 @@
 
 declare(strict_types = 1);
 
+// Prevent Redis from loading in PHPUnit tests.
+if (defined('BOOTSTRAP_IS_PHPUNIT')) {
+  return;
+}
+
 $settings['container_yamls'][] = __DIR__ . '/services.redis.yml';
 
 $settings['cache']['default'] = 'cache.backend.redis';
